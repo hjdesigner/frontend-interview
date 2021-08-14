e107c4af51a62011bdd4f79a2f59fdb31c700f49
Henrique Melanda <hjdesigner23@gmail.com>
2021-08-14 17:12:41 -0300

    FEATURE GAME FOUR IN A ROW
    - Created game 4 in a row;
    - Refactored in the code to reuse the scoring, timing and winner methods to use in this game as well;
    - Duplicate the statistics section so that each game has its own results;
    Made the game's responsiveness;
    - Created the event to change game and statistics accordingly click on the menu;
    - Created a method to lock the menu while the player is playing;

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,21 +6,30 @@
   const winsPlayOneMobile = document.querySelector('[data-id="wins-play-one-mobile"]');
   const winsPlayTwoMobile = document.querySelector('[data-id="wins-play-two-mobile"]');
   const reset = document.querySelector('[data-id="reset-tic-three-by-three"]');
+  const resetFourInARow = document.querySelector('[data-id="reset-for-in-a-row"]');
   const actions = document.querySelector('[data-id="actions-tic-three-by-three"]');
+  const actionsFourInARow = document.querySelector('[data-id="actions-for-in-a-row"]');
   const playTurn = document.querySelector('[data-id="turn__three_by_three"]');
   const playOneWinner = document.querySelector('[data-id="one-winner"]');
   const playTwoWinner = document.querySelector('[data-id="two-winner"]');
   const historyResults = document.querySelectorAll('.data__historyresults');
+  const historyResultsFourInARow = document.querySelectorAll('.data__historyresults-four-in-a-row');
   const matchResults = document.querySelectorAll('.data__matchresults');
+  const matchResultsFourInARow = document.querySelectorAll('.data__matchresults-four-in-a-row');
   const winsPercentagePlayOne = document.querySelector('[data-id="wins-player-one"]');
+  const winsPercentagePlayOneFourInARow = document.querySelector('[data-id="wins-player-one-four-in-a-row"]');
   const winsPercentagePlayTwo = document.querySelector('[data-id="wins-player-two"]');
+  const winsPercentagePlayTwoFourInARow = document.querySelector('[data-id="wins-player-two-four-in-a-row"]');
   const defeatsPercentagePlayOne = document.querySelector('[data-id="defeats-player-one"]');
+  const defeatsPercentagePlayOneFourInARow = document.querySelector('[data-id="defeats-player-one-four-in-a-row"]');
   const defeatsPercentagePlayTwo = document.querySelector('[data-id="defeats-player-two"]');
+  const defeatsPercentagePlayTwoFourInARow = document.querySelector('[data-id="defeats-player-two-four-in-a-row"]');
   const timeHour = document.querySelector('[data-id="hour"]');
   const timeMinute = document.querySelector('[data-id="minute"]');
   const timeSecond = document.querySelector('[data-id="second"]');
   const gameTime = document.querySelector('[data-id="game-time"]');
   const totalTime = document.querySelector('[data-id="total-time"]');
+  const totalTimeFourInRow = document.querySelector('[data-id="total-time-four-in-a-row"]');
   const trophyOne = document.querySelector('[data-id="games-trophy-one"]');
   const trophyTwo = document.querySelector('[data-id="games-trophy-two"]');
   const trophyOneMobile = document.querySelector('[data-id="games-trophy-one-mobile"]');
@@ -28,6 +37,13 @@
   const data = document.querySelector('.data');
   const valueSubscribe = document.querySelector('[data-id="subscribe"]');
   const sendSubscribe = document.querySelector('[data-id="send-subscribe"]');
+  const buttonTicTacToe = document.querySelector('[data-id="tic-tac-toe"]');
+  const buttonFourInARow = document.querySelector('[data-id="four-in-a-row"]');
+  const gameTicTacToe = document.querySelector('[data-id="game-tic-three-by-three"]');
+  const gameForInARow = document.querySelector('[data-id="game-four-in-a-row"]');
+  const columnsFourInARow = document.querySelectorAll('.game__for_in_a_row__column');
+  const dataThreeByThree = document.querySelector('[data-id="data-three-by-three"]');
+  const dataFourInARow = document.querySelector('[data-id="data-four-in-a-row"]');
   const winningConditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -43,6 +59,13 @@
   let numberOfWinsThreeByThreePlayerTwo = 0;
   let numberOfLossesThreeByThreePlayerOne = 0;
   let numberOfLossesThreeByThreePlayerTwo = 0;
+
+  let numberOfMatchesFourInARow = 0;
+  let numberOfWinsFourInARowPlayerOne = 0;
+  let numberOfWinsFourInARowPlayerTwo = 0;
+  let numberOfLossesFourInARowPlayerOne = 0;
+  let numberOfLossesFourInARowPlayerTwo = 0;
+
   let hour = 0;
   let minute = 0;
   let second = 0;
@@ -52,6 +75,12 @@
   let currentPlayer = "X";
   let gameState = ["", "", "", "", "", "", "", "", ""];
   let startTime = false;
+
+  let gameBoardFourInARow = {};
+  let currentPlayerFourInARow = 'red';
+  let numRowsFourInARow = 6;
+  let numColsFourInARow = 7;
+  let numTurnsFourInARow = 0;
   
 
   function transparentHeader() {
@@ -142,6 +171,107 @@
     currentPlayer = currentPlayer === "X" ? "O" : "X";
     playTurn.innerHTML = `It's player ${currentPlayer === 'X' ? '1' : '2'}'s turn`;
   }
+
+  function publishingDataFourInARow(player) {
+    const valueGameTime = gameTime.innerText;
+    const valueTotalTime = totalTimeFourInRow.innerHTML;
+    numberOfMatchesFourInARow = numberOfMatchesFourInARow + 1;
+
+    const valueWinsPercentagePlayOne = (numberOfWinsFourInARowPlayerOne/numberOfMatchesFourInARow*100).toFixed();
+    const valueWinsPercentagePlayTwo = (numberOfWinsFourInARowPlayerTwo/numberOfMatchesFourInARow*100).toFixed();
+    const valueDefeatsPercentagePlayOne = (numberOfLossesFourInARowPlayerOne/numberOfMatchesFourInARow*100).toFixed();
+    const valueDefeatsPercentagePlayTwo = (numberOfLossesFourInARowPlayerTwo/numberOfMatchesFourInARow*100).toFixed();
+
+    winsPercentagePlayOneFourInARow.innerHTML = `${valueWinsPercentagePlayOne}%`;
+    winsPercentagePlayTwoFourInARow.innerHTML = `${valueWinsPercentagePlayTwo}%`;
+    defeatsPercentagePlayOneFourInARow.innerHTML = `${valueDefeatsPercentagePlayOne}%`;
+    defeatsPercentagePlayTwoFourInARow.innerHTML = `${valueDefeatsPercentagePlayTwo}%`;
+
+    if (valueWinsPercentagePlayOne <= 33) {
+      winsPercentagePlayOneFourInARow.classList.remove('warning');
+      winsPercentagePlayOneFourInARow.classList.remove('success');
+      winsPercentagePlayOneFourInARow.classList.add('danger');
+    }
+    if (valueWinsPercentagePlayOne > 33 && valueWinsPercentagePlayOne <= 66) {
+      winsPercentagePlayOneFourInARow.classList.remove('danger');
+      winsPercentagePlayOneFourInARow.classList.remove('success');
+      winsPercentagePlayOneFourInARow.classList.add('warning');
+    }
+    if (valueWinsPercentagePlayOne > 66) {
+      winsPercentagePlayOneFourInARow.classList.remove('warning');
+      winsPercentagePlayOneFourInARow.classList.remove('danger');
+      winsPercentagePlayOneFourInARow.classList.add('success');
+    }
+
+    if (valueWinsPercentagePlayTwo <= 33) {
+      winsPercentagePlayTwoFourInARow.classList.remove('warning');
+      winsPercentagePlayTwoFourInARow.classList.remove('success');
+      winsPercentagePlayTwoFourInARow.classList.add('danger');
+    }
+    if (valueWinsPercentagePlayTwo > 33 && valueWinsPercentagePlayTwo <= 66) {
+      winsPercentagePlayTwoFourInARow.classList.remove('danger');
+      winsPercentagePlayTwoFourInARow.classList.remove('success');
+      winsPercentagePlayTwoFourInARow.classList.add('warning');
+    }
+    if (valueWinsPercentagePlayTwo > 66) {
+      winsPercentagePlayTwoFourInARow.classList.remove('warning');
+      winsPercentagePlayTwoFourInARow.classList.remove('danger');
+      winsPercentagePlayTwoFourInARow.classList.add('success');
+    }
+
+    if (valueDefeatsPercentagePlayOne <= 33) {
+      defeatsPercentagePlayOneFourInARow.classList.remove('warning');
+      defeatsPercentagePlayOneFourInARow.classList.remove('danger');
+      defeatsPercentagePlayOneFourInARow.classList.add('success');
+    }
+    if (valueDefeatsPercentagePlayOne > 33 && valueDefeatsPercentagePlayOne <= 66) {
+      defeatsPercentagePlayOneFourInARow.classList.remove('danger');
+      defeatsPercentagePlayOneFourInARow.classList.remove('success');
+      defeatsPercentagePlayOneFourInARow.classList.add('warning');
+    }
+    if (valueDefeatsPercentagePlayOne > 66) {
+      defeatsPercentagePlayOneFourInARow.classList.remove('warning');
+      defeatsPercentagePlayOneFourInARow.classList.remove('success');
+      defeatsPercentagePlayOneFourInARow.classList.add('danger');
+    }
+
+    if (valueDefeatsPercentagePlayTwo <= 33) {
+      defeatsPercentagePlayTwoFourInARow.classList.remove('warning');
+      defeatsPercentagePlayTwoFourInARow.classList.remove('danger');
+      defeatsPercentagePlayTwoFourInARow.classList.add('success');
+    }
+    if (valueDefeatsPercentagePlayTwo > 33 && valueDefeatsPercentagePlayTwo <= 66) {
+      defeatsPercentagePlayTwoFourInARow.classList.remove('danger');
+      defeatsPercentagePlayTwoFourInARow.classList.remove('success');
+      defeatsPercentagePlayTwoFourInARow.classList.add('warning');
+    }
+    if (valueDefeatsPercentagePlayTwo > 66) {
+      defeatsPercentagePlayTwoFourInARow.classList.remove('warning');
+      defeatsPercentagePlayTwoFourInARow.classList.remove('success');
+      defeatsPercentagePlayTwoFourInARow.classList.add('danger');
+    }
+
+    totalTimeFourInRow.innerHTML = addTimes(valueGameTime, valueTotalTime);
+
+    for (let i = 0; i < historyResultsFourInARow.length; i++) {
+      if (historyResultsFourInARow[i].innerHTML === '') {
+        historyResultsFourInARow[i].innerHTML = player;
+        break;
+      }
+    }
+
+    for (let i = 0; i < matchResultsFourInARow.length; i++) {
+      if (!matchResultsFourInARow[i].classList.contains('data__matchresults_active')) {
+        matchResultsFourInARow[i].classList.add('data__matchresults_active');
+        break;
+      }
+    }
+    data.scrollIntoView({
+      behavior: 'smooth'
+    });
+    
+  }
+
 
   function publishingData(player) {
     const valueGameTime = gameTime.innerText;
@@ -243,6 +373,21 @@
     
   }
 
+  function playerWin(player, valueWinsPlayOne, valueWinsPlayTwo, valueWinsPlayOneMobile, valueWinsPlayTwoMobile) {
+    if (player === 1) {
+      winsPlayOne.innerHTML = parseInt(valueWinsPlayOne) + 1
+      winsPlayOneMobile.innerHTML = parseInt(valueWinsPlayOneMobile) + 1
+      trophyOne.classList.add('active');
+      trophyOneMobile.classList.add('active');
+    }
+    if (player === 2) {
+      winsPlayTwo.innerHTML = parseInt(valueWinsPlayTwo) + 1;
+      winsPlayTwoMobile.innerHTML = parseInt(valueWinsPlayTwoMobile) + 1;
+      trophyTwo.classList.add('active');
+      trophyTwoMobile.classList.add('active');
+    }
+  }
+
   function handleResultValidation() {
     const valueWinsPlayOne = winsPlayOne.innerHTML;
     const valueWinsPlayTwo = winsPlayTwo.innerHTML;
@@ -266,15 +411,9 @@
 
     if (roundWon) {
       if (currentPlayer === 'X') {
-        winsPlayOne.innerHTML = parseInt(valueWinsPlayOne) + 1
-        winsPlayOneMobile.innerHTML = parseInt(valueWinsPlayOneMobile) + 1
-        trophyOne.classList.add('active');
-        trophyOneMobile.classList.add('active');
+        playerWin(1, valueWinsPlayOne, valueWinsPlayTwo, valueWinsPlayOneMobile, valueWinsPlayTwoMobile);
       } else {
-        winsPlayTwo.innerHTML = parseInt(valueWinsPlayTwo) + 1;
-        winsPlayTwoMobile.innerHTML = parseInt(valueWinsPlayTwoMobile) + 1;
-        trophyTwo.classList.add('active');
-        trophyTwoMobile.classList.add('active');
+        playerWin(2, valueWinsPlayOne, valueWinsPlayTwo, valueWinsPlayOneMobile, valueWinsPlayTwoMobile);
       }
       actions.classList.add('actions--active');
       playTurn.innerHTML = '';
@@ -317,6 +456,8 @@
 
     if (!startTime) {
       startStopWatch();
+      buttonFourInARow.disabled = true;
+      buttonTicTacToe.disabled = true;
       startTime = true;
     }
     if (gameState[clickedCellIndex] !== "" || !gameActive) {
@@ -327,6 +468,7 @@
     handleResultValidation();
   
   }
+
   function handleRestartGame() {
     const valueWinsPlayOne = winsPlayOne.innerHTML;
     const valueWinsPlayTwo = winsPlayTwo.innerHTML;
@@ -338,16 +480,21 @@
       winsPlayTwo.innerHTML = 0;
       winsPlayOneMobile.innerHTML = 0;
       winsPlayTwoMobile.innerHTML = 0;
+      buttonFourInARow.disabled = false;
+      buttonTicTacToe.disabled = false;
       resetStopWatch();
     }
+    clearBoard();
     playTurn.innerHTML = '';
     gameActive = true;
     currentPlayer = "X";
+    currentPlayerFourInARow = 'red';
     gameState = ["", "", "", "", "", "", "", "", ""];
     trophyOne.classList.remove('active');
     trophyTwo.classList.remove('active');
     trophyOneMobile.classList.remove('active');
     trophyTwoMobile.classList.remove('active');
+    actionsFourInARow.classList.remove('actions--active');
     document.querySelector('[data-id="actions-tic-three-by-three"]').classList.remove('actions--active');
     document.querySelectorAll('.game__tic__three_by_three__cell')
             .forEach(cell => {
@@ -370,8 +517,170 @@
     })
   }
 
+  function changeGame() {
+    buttonTicTacToe.addEventListener('click', (e) => {
+      e.preventDefault();
+      buttonFourInARow.classList.remove('game__button--active');
+      buttonTicTacToe.classList.add('game__button--active');
+      gameForInARow.classList.remove('active');
+      gameTicTacToe.classList.add('active');
+      dataFourInARow.classList.remove('active');
+      dataThreeByThree.classList.add('active');
+    });
+    buttonFourInARow.addEventListener('click', (e) => {
+      e.preventDefault();
+      buttonTicTacToe.classList.remove('game__button--active');
+      buttonFourInARow.classList.add('game__button--active');
+      gameTicTacToe.classList.remove('active');
+      gameForInARow.classList.add('active');
+      dataThreeByThree.classList.remove('active');
+      dataFourInARow.classList.add('active');
+    });
+  }
+
+
+  function clearBoard() {
+		Array.prototype.forEach.call(document.querySelectorAll('.circle'), function(piece) {
+      piece.setAttribute('class', 'circle free');
+    });
+    gameBoardFourInARow = {};
+    for(var x = 0; x <= numRowsFourInARow; x++) {
+      gameBoardFourInARow[x] = {};
+      for(var y = 0; y <= numColsFourInARow; y++) {
+        gameBoardFourInARow[x][y] = 'free';
+      }
+    }
+
+    numTurnsFourInARow = 0;
+    return gameBoardFourInARow;
+  };
+
+  function isBounds(x, y) {
+    return (gameBoardFourInARow.hasOwnProperty(x) && typeof gameBoardFourInARow[x][y] !== 'undefined');
+  };
+
+  function checkDirection(currentX, currentY, direction) {
+    let chainLength;
+    let directions;
+    
+    directions = {
+      horizontal: [
+        [0, -1], [0, 1]
+      ],
+      vertical: [
+        [-1, 0], [1, 0]
+      ],
+      diagonal: [
+        [-1, -1], [1, 1], [-1, 1], [1, -1]
+      ]
+    };
+    
+    chainLength = 1;
+    
+    directions[direction].forEach(function(coords) {
+      let i = 1;
+      while( isBounds(currentX + (coords[0] * i), currentY + (coords[1] * i)) && 
+        (gameBoardFourInARow[currentX + (coords[0] * i)][currentY + (coords[1] * i)] === currentPlayerFourInARow)
+      ) {
+        chainLength = chainLength + 1; 
+        i = i + 1; 
+      };
+    });
+    return (chainLength >= 4);    
+  };
+
+  function isWinner(currentX, currentY) {
+    return checkDirection(currentX, currentY, 'vertical') || 
+      checkDirection(currentX, currentY, 'diagonal') || 
+      checkDirection(currentX, currentY, 'horizontal');
+  };
+
+  function markNextFree(x) {
+    const valueWinsPlayOne = winsPlayOne.innerHTML;
+    const valueWinsPlayTwo = winsPlayTwo.innerHTML;
+    const valueWinsPlayOneMobile = winsPlayOneMobile.innerHTML;
+    const valueWinsPlayTwoMobile = winsPlayTwoMobile.innerHTML;
+    let nextY = false;
+
+    for (let y = 0; y < numRowsFourInARow; y++) {
+      if(gameBoardFourInARow[x][y] === 'free') {
+        nextY = y;
+        break;
+      }
+    }
+    if (nextY === false) {
+      return false;
+    }
+    gameBoardFourInARow[x][nextY] = currentPlayerFourInARow;
+
+    document.querySelector('#column-'+x+' .row-'+nextY+' .circle').setAttribute(
+      'class', `circle ${currentPlayerFourInARow}`
+    );
+
+    if (isWinner(parseInt(x), nextY)) {
+      currentPlayerFourInARow === 'red' ? 
+        playerWin(1, valueWinsPlayOne, valueWinsPlayTwo, valueWinsPlayOneMobile, valueWinsPlayTwoMobile) : 
+        playerWin(2, valueWinsPlayOne, valueWinsPlayTwo, valueWinsPlayOneMobile, valueWinsPlayTwoMobile);
+      actionsFourInARow.classList.add('actions--active');
+
+      playTurn.innerHTML = '';
+      gameActive = false;
+      
+      if (valueWinsPlayOne === '4' && currentPlayerFourInARow === 'red') {
+        playOneWinner.classList.add('active');
+        
+        numberOfWinsFourInARowPlayerOne = numberOfWinsFourInARowPlayerOne + 1
+        numberOfLossesFourInARowPlayerTwo = numberOfLossesFourInARowPlayerTwo + 1
+        publishingDataFourInARow('P1');
+        pause();
+      }
+      if (valueWinsPlayTwo === '4' && currentPlayerFourInARow === 'yellow') {
+        playTwoWinner.classList.add('active');
+        
+        numberOfWinsFourInARowPlayerTwo = numberOfWinsFourInARowPlayerTwo + 1
+        numberOfLossesFourInARowPlayerOne = numberOfLossesFourInARowPlayerOne + 1
+        publishingDataFourInARow('P2');
+        pause();
+      }
+      return;
+    }
+
+    numTurnsFourInARow = numTurnsFourInARow + 1;
+
+    if (numTurnsFourInARow >= numRowsFourInARow * numColsFourInARow) {
+      playTurn.innerHTML = 'This round ended in a draw';
+      actionsFourInARow.classList.add('actions--active');
+      return;
+    }
+
+    currentPlayerFourInARow = currentPlayerFourInARow === 'red' ? 'yellow' : 'red';
+    playTurn.innerHTML = `It's player ${currentPlayerFourInARow === 'red' ? '1' : '2'}'s turn`;
+  }
+
+  function initFourInARow() {
+    columnsFourInARow.forEach(col => col.addEventListener('click', () => {
+      if (!startTime) {
+        startStopWatch();
+        buttonFourInARow.disabled = true;
+        buttonTicTacToe.disabled = true;
+        startTime = true;
+      }
+      markNextFree(col.getAttribute('data-x'))
+    }))
+
+    for(let x = 0; x <= numRowsFourInARow; x++) {
+      gameBoardFourInARow[x] = {};
+      for(var y = 0; y <= numColsFourInARow; y++) {
+        gameBoardFourInARow[x][y] = 'free';
+      }
+    }
+  }
+
   cells.forEach(cell => cell.addEventListener('click', handleCellClick));
   reset.addEventListener('click', handleRestartGame);
+  resetFourInARow.addEventListener('click', handleRestartGame);
   transparentHeader();
   validSubscribe();
+  changeGame();
+  initFourInARow();
 })()
